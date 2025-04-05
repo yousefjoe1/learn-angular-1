79b1fedd026d6d32f1b42fa8299e582d02654399
yousefjoe1 <yousefmahmoud150@gmail.com>
2025-04-05 17:53:10 +0200

    adding router link to navigate betw pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { CounterComponent } from './components/counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  HeaderComponent, CounterComponent],
-  // templateUrl: './app.component.html',
+  imports: [RouterOutlet, HeaderComponent],
   template: `
     <app-header />
 
     <main
-      class="p-3 bg-gray-300 h-screen m-3 shadow-lg rounded-2xl flex justify-center items-center"
+      class="p-3 bg-gray-300 h-screen m-3 shadow-lg rounded-2xl"
     >
-      <!-- <div class="rounded-2xl bg-gray-50 shadow-lg p-3 md:w-1/2 w-full">
-        <button (click)="generateFunc()"  type="button" title="Generate Advice" class="metal mx-auto lg:w-32 block ">
-          <span class="text">Generate</span>
-        </button>
-      </div> -->
-      <app-counter />
+      <router-outlet />
     </main>
-    <router-outlet>
   `,
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Learn Angular 2025';
-
-  // async generateFunc(): Promise<void> {
-  //   try {
-  //     const response = await fetch('https://zenquotes.io/api/random')
-  //     console.log("🚀 ~ AppComponent ~ generateFunc ~ response:", response)
-  //     const res = await response.json();
-  //     console.log("🚀 ~ AppComponent ~ generateFunc ~ res:", res)
-  //     // console.log("🚀 ~ Advice:", slip);
-  //   } catch (error) {
-  //     console.error('Error fetching advice:', error);
-  //   }
-  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 
-export const routes: Routes = [{
-    path: "aboutme",
+export const routes: Routes = [
+    {
+        path: "aboutme",
+        pathMatch: "full",
+        loadComponent: () => import("./aboutme/aboutme.component").then(m=> m.AboutmeComponent)
+    },
+    {
+    path: "",
     // component: AboutmeComponent,
-    loadComponent: () => import("./aboutme/aboutme.component").then(m=> m.AboutmeComponent)
-}];
+    loadComponent: () => import("./home/home.component").then(m=> m.HomeComponent)
+},
+
+];
