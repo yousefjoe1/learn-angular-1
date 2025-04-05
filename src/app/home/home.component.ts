4ccb7324ef7ecc3c28e4764ea8cdb5eaee5b24e1
yousefjoe1 <yousefmahmoud150@gmail.com>
2025-04-05 21:01:32 +0200

    test api call with fetch and services - make routing and components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { QuotesService } from '../services/quotes.service';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  quotes = inject(QuotesService)
+
+  ngOnInit(): void {// when component is loaded
+      console.log(this.quotes.getQuotes());
+      this.quotes.getQuotes().pipe(
+        catchError(err => {
+          console.log("🚀 ~ HomeComponent ~ ngOnInit ~ error:", err)
+          throw err;
+
+        })
+      ).subscribe(v=> {
+        console.log("🚀 ~ HomeComponent ~ ngOnInit ~ v:", v)
+        
+      })
+  }
 }
---
+++ src/app/types/quote.type.ts
@@ -0,0 +1,5 @@
+ export interface quote {
+    q: string;
+    a: string;
+    h: string;
+ }
