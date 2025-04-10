a21f3e6d0f21fa17c7646bf33d75cf864905f8e4
yousefjoe1 <yousefmahmoud150@gmail.com>
2025-04-10 22:20:11 +0200

    adding hero section v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   template: `
+    <app-navbar />
 
-    <main
-      class="p-3 bg-gray-300 h-screen m-3 shadow-lg rounded-2xl"
-    >
       <router-outlet />
-    </main>
   `,
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        loadComponent: () => import("./home/home.component").then(m=> m.HomeComponent)
+    },
+    // {
+    //     path: "aboutme",
+    //     loadComponent: () => import("./components/aboutme/aboutme.component").then(m=> m.AboutmeComponent)
+    // },
+];
